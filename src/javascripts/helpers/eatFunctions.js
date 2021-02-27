413502cfc52e32138a5aa9d23cd7f450d49cff64
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-02-26 20:16:34 -0600

    fully modularized js portion of eats quadrant

--- src/javascripts/helpers/eatFunctions.js
+++ src/javascripts/helpers/eatFunctions.js
@@ -1,11 +1,11 @@
-let full = 40;
-
 function wholeFoods() {
+  let full = Number(document.querySelector('#full').innerHTML);
   full = Math.min(full + 10, 100);
   document.querySelector('#full').innerHTML = `${full}`;
 }
 
 function junkFood() {
+  let full = Number(document.querySelector('#full').innerHTML);
   full = Math.min(full + 3, 100);
   document.querySelector('#full').innerHTML = `${full}`;
 }
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,21 +1,5 @@
 import '../styles/main.scss';
-
-let full = 100;
-
-function wholeFoods() {
-  full = Math.min(full + 10, 100);
-  document.querySelector('#full').innerHTML = `${full}`;
-}
-
-function junkFood() {
-  full = Math.min(full + 3, 100);
-  document.querySelector('#full').innerHTML = `${full}`;
-}
-
-function buttonEvents() {
-  document.querySelector('#wholeFoods').addEventListener('click', wholeFoods);
-  document.querySelector('#junkFood').addEventListener('click', junkFood);
-}
+import buttonEvents from './helpers/buttonEvents';
 
 function init() {
   buttonEvents();
