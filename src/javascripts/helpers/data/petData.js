8aac55e13189ad4745b7c29b6670a6d415a71b87
Greenfin17 <jmaple@yahoo.com>
2021-03-02 21:01:46 -0600

    updating image link

--- src/javascripts/helpers/data/petData.js
+++ src/javascripts/helpers/data/petData.js
@@ -2,8 +2,7 @@
 
 const defaultPetName = 'Fido';
 const defaultPetUrl = 'https://www.clipartkey.com/mpngs/m/11-119414_easy-clipart-dog-easy-simple-dog-drawing.png';
-const kaputUrl = 'http://clipart-library.com/images/ki8ozrkeT.jpg';
-
+const kaputUrl = 'https://www.kindpng.com/picc/m/426-4264032_how-to-draw-tombstone-tombstone-drawing-easy-hd.png';
 let petName = defaultPetName;
 let petUrl = defaultPetUrl;
 
